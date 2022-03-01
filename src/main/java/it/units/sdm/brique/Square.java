8bc4f936c5b50134bc49e487cc1381e5d98f96ef
Alnu73 <axel8744@gmail.com>
2022-03-01 16:44:13 +0100

    Added Square class logic

--- src/main/java/it/units/sdm/brique/Square.java
+++ src/main/java/it/units/sdm/brique/Square.java
@@ -1,7 +1,27 @@
 package it.units.sdm.brique;
 
 public class Square {
-  Coordinate coordinate;
-  Color color;
-  Stone stone;
+  private Coordinate coordinate;
+  private Color color;
+  private Stone stone;
+
+  public Square(Coordinate coordinate, Color color, Stone stone) {
+    this.coordinate = coordinate;
+    this.color = color;
+    this.stone = stone;
+  }
+
+  public Coordinate getCoordinate() {
+    return coordinate;
+  }
+
+  public Color getColor() {
+    return color;
+  }
+
+  public Stone getStone() {
+    return stone;
+  }
+
 }
+
