4f7ac3efec3d403dfa87ab0b9a7f88d30c4b0fd4
Antonio Procentese <anto.proce@gmail.com>
2022-03-01 17:13:45 +0100

    Replaced single test with parametrized tests

--- src/test/java/it/units/sdm/brique/TestCoordinates.java
+++ src/test/java/it/units/sdm/brique/TestCoordinates.java
@@ -1,17 +1,23 @@
 package it.units.sdm.brique;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestCoordinates {
-
-    Coordinate c = new Coordinate(3,4);
-    @Test
-    void testX() {
-        assertEquals(3, c.getX());
-    }
-    void testY() {
-        assertEquals(4, c.getY());
+    
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15})
+    void test_X_Coordinates(int number){
+        Coordinate c = new Coordinate (number,0);
+        assertEquals(number, c.getX());
     }
 
-
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15})
+    void test_Y_Coordinates(int number){
+        Coordinate c = new Coordinate (0,number);
+        assertEquals(number, c.getY());
+    }
 }
