37fcc42404218f3962c7bc207f1ed5d2d1e9af1b
riccardozulla <riccardozulla@gmail.com>
2022-03-02 10:03:53 +0100

    refactored testNewStoneColor

--- src/test/java/it/units/sdm/brique/TestStone.java
+++ src/test/java/it/units/sdm/brique/TestStone.java
@@ -1,26 +1,24 @@
 package it.units.sdm.brique;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStone {
-  @Test
-  void testNewBlackStone() {
-    Stone stone = new Stone(Color.BLACK);
-    assertEquals(Color.BLACK, stone.getColor());
-  }
-
-  @Test
-  void testNewWhiteStone() {
-    Stone stone = new Stone(Color.WHITE);
-    assertEquals(Color.WHITE, stone.getColor());
+  @ParameterizedTest
+  @EnumSource(value = Color.class)
+  void testNewStoneColor(Color color) {
+    Stone stone = new Stone(color);
+    assertEquals(color, stone.getColor());
   }
 
   @Test
   void testSetBlackColor() {
     Stone stone = new Stone(Color.WHITE);
     stone.setColor(Color.BLACK);
-    assertEquals(Color.BLACK,stone.getColor());
+    assertEquals(Color.BLACK, stone.getColor());
   }
 
   @Test
