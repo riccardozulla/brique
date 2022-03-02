079f9c297afe6f17495aebab04efe32cb720343b
riccardozulla <riccardozulla@gmail.com>
2022-03-02 15:29:08 +0100

    first implementation of addStoneAndUpdateBoard method

--- src/main/java/it/units/sdm/brique/Game.java
+++ src/main/java/it/units/sdm/brique/Game.java
@@ -1,34 +1,47 @@
 package it.units.sdm.brique;
 
 public class Game {
-    private static final int BOARD_SIZE = 15;
-    private Status status = Status.RUNNING;
-    private final Player[] players;
-    private final Board gameBoard;
-    private Player nextMove;
+  private static final int BOARD_SIZE = 15;
+  private Status status = Status.RUNNING;
+  private final Player[] players;
+  private final Board gameBoard;
+  private Player nextMove;
 
-    public Game(Player player1, Player player2){
-        this.players = new Player[]{player1, player2};
-        this.gameBoard = new Board(BOARD_SIZE);
-    }
+  public Game(Player player1, Player player2) {
+    this.players = new Player[]{player1, player2};
+    this.gameBoard = new Board(BOARD_SIZE);
+  }
 
-    public Status getStatus() {
-        return status;
-    }
+  public Status getStatus() {
+    return status;
+  }
 
-    public Player getNextMove() {
-        return nextMove;
-    }
+  public Player getNextMove() {
+    return nextMove;
+  }
 
-    public Player[] getPlayers() {
-        return players;
-    }
+  public Player[] getPlayers() {
+    return players;
+  }
 
-    public Board getGameBoard() {
-        return gameBoard;
-    }
+  public Board getGameBoard() {
+    return gameBoard;
+  }
 
-    public void addStone(int x, int y, Color color) {
-        gameBoard.getSquare(x, y).setStone(new Stone(color));
+  public void addStone(int x, int y, Color color) {
+    gameBoard.getSquare(x, y).setStone(new Stone(color));
+  }
+
+  public void addStoneAndUpdateBoard(int x, int y, Color color) {
+    addStone(x, y, color);
+    if (getGameBoard().getSquare(x + 1, y - 1).getIsOccupied()) {
+      if (getGameBoard().getSquare(x + 1, y - 1).getStone().getColor() == color) {
+        if (getGameBoard().getSquare(x, y).getColor() == Color.BLACK) {
+          addStone(x + 1, y, color);
+        } else {
+          addStone(x, y - 1, color);
+        }
+      }
     }
+  }
 }
