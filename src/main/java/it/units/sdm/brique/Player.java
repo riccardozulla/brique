180b7d95f975728a8f9977dc4459efd3a2ddfb5e
riccardozulla <riccardozulla@gmail.com>
2022-03-17 18:59:03 +0100

    added equals and hashCode methods in Player class

--- src/main/java/it/units/sdm/brique/Player.java
+++ src/main/java/it/units/sdm/brique/Player.java
@@ -17,4 +17,21 @@ public class Player {
         return stoneColor;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (!nickname.equals(player.nickname)) return false;
+        return stoneColor == player.stoneColor;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nickname.hashCode();
+        result = 31 * result + stoneColor.hashCode();
+        return result;
+    }
 }
