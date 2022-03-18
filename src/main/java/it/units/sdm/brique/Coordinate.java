b46526d34527f8e2b1c18056cbd2d0c328f73325
Antonio Procentese <anto.proce@gmail.com>
2022-03-18 12:43:17 +0100

    Coordinate has now a toString method

--- src/main/java/it/units/sdm/brique/Coordinate.java
+++ src/main/java/it/units/sdm/brique/Coordinate.java
@@ -15,6 +15,14 @@ public class Coordinate {
   public int getY() {
     return y;
   }
+
+  @Override
+  public String toString() {
+    return "Coordinate{" +
+            "x=" + x +
+            ", y=" + y +
+            '}';
+  }
 }
 
 
