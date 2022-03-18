1b74e2e1b70ccb0fc44310958df2ae5712b09c39
Antonio Procentese <anto.proce@gmail.com>
2022-03-18 10:57:19 +0100

    BoardIndexOutOfBoundException class added

---
+++ src/main/java/it/units/sdm/brique/exceptions/BoardIndexOutOfBoundException.java
@@ -0,0 +1,7 @@
+package it.units.sdm.brique.exceptions;
+
+public class BoardIndexOutOfBoundException extends RuntimeException{
+    public BoardIndexOutOfBoundException(String message){
+        super(message);
+    }
+}
