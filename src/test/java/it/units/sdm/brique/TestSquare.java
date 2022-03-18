fa93aac3aa6a3eb9b35a4412cbf5579e1dde80dd
riccardozulla <riccardozulla@gmail.com>
2022-03-18 10:18:22 +0100

    modified constructor according to previous commits

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSquare {
-    private final Square square = new Square(Color.BLACK);
+    private final Square square = new Square(new Coordinate(0,0), Color.BLACK);
 
     @Test
     void toggleTestToFalse() {
