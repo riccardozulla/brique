1432337ddbf66d4e1eddb9f9bac807b3598d118f
Antonio Procentese <anto.proce@gmail.com>
2022-03-25 12:04:07 +0100

    added test for checking winning path (red build, the test fails)

--- src/main/java/it/units/sdm/brique/ClusterSet.java
+++ src/main/java/it/units/sdm/brique/ClusterSet.java
@@ -7,17 +7,21 @@ import java.util.Set;
 
 public class ClusterSet extends UnionFind<Square> {
 
-  Color color;
+    Color color;
 
-  public ClusterSet(Set<Square> elements, Color stonesColor) {
-    super(elements);
-    this.color = stonesColor;
-  }
+    public ClusterSet(Set<Square> elements, Color stonesColor) {
+        super(elements);
+        this.color = stonesColor;
+    }
 
-  public void composeClusters() {
-    Set<Square> elements = getParentMap().keySet();
-    Sets.cartesianProduct(elements, elements).stream().
-            filter(e -> Square.manhattanSquareDistance(e.get(0), e.get(1)) == 1).
-            forEach(e -> union(e.get(0), e.get(1)));
-  }
+    public void composeClusters() {
+        Set<Square> elements = getParentMap().keySet();
+        Sets.cartesianProduct(elements, elements).stream().
+                filter(e -> Square.manhattanSquareDistance(e.get(0), e.get(1)) == 1).
+                forEach(e -> union(e.get(0), e.get(1)));
+    }
+
+    public boolean winningPath() {
+        return false;
+    }
 }
--- src/test/java/it/units/sdm/brique/TestClusterSet.java
+++ src/test/java/it/units/sdm/brique/TestClusterSet.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestClusterSet {
 
@@ -23,6 +24,28 @@ public class TestClusterSet {
   void composeClusters() {
     clusterSet.composeClusters();
     assertEquals(1, clusterSet.numberOfSets());
+  }
 
+  @Test
+  void winningPath(){
+    Square s2 = new Square(new Coordinate(1, 1), Color.WHITE);
+    Square s3 = new Square(new Coordinate(1, 2), Color.BLACK);
+    Square s4 = new Square(new Coordinate(1, 3), Color.WHITE);
+    Square s5 = new Square(new Coordinate(1, 4), Color.BLACK);
+    Square s6 = new Square(new Coordinate(1, 5), Color.WHITE);
+    Square s7 = new Square(new Coordinate(1, 6), Color.BLACK);
+    Square s8 = new Square(new Coordinate(1, 7), Color.WHITE);
+    Square s9 = new Square(new Coordinate(1, 8), Color.BLACK);
+    Square s10 = new Square(new Coordinate(1, 9), Color.BLACK);
+    Square s11 = new Square(new Coordinate(1, 10), Color.BLACK);
+    Square s12 = new Square(new Coordinate(1, 11), Color.BLACK);
+    Square s13 = new Square(new Coordinate(1, 12), Color.BLACK);
+    Square s14 = new Square(new Coordinate(1, 13), Color.BLACK);
+    Square s15 = new Square(new Coordinate(1, 14), Color.BLACK);
+    ClusterSet clusterSet = new ClusterSet(Set.of(s1, s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15), Color.BLACK);
+    clusterSet.composeClusters();
+    assertEquals(1, clusterSet.numberOfSets());
+    assertTrue(winningPath());
+    //a
   }
 }
