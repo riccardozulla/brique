3cdda862c650637dacdd04ecf917e05bac53e1e2
Alnu73 <axel8744@gmail.com>
2022-03-28 18:01:59 +0200

    Initialized UI

---
+++ src/main/java/it/units/sdm/brique/Brique.java
@@ -0,0 +1,23 @@
+package it.units.sdm.brique;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Brique extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Brique");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
