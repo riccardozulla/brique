11c57222b4acbe58821d38def8deb145456cf405
Alnu73 <axel8744@gmail.com>
2022-03-29 16:35:59 +0200

    Added drawSquare in GraphicSquare

--- src/main/java/it/units/sdm/brique/ui/GraphicSquare.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicSquare.java
@@ -1,6 +1,9 @@
 package it.units.sdm.brique.ui;
 
+import it.units.sdm.brique.model.Board;
+import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Square;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 
 public class GraphicSquare extends Rectangle {
@@ -8,6 +11,16 @@ public class GraphicSquare extends Rectangle {
     private Square square;
 
     public GraphicSquare(Square square) {
+        super();
         this.square = square;
+    }
+
+    public void drawSquare(Pane pane) {
+        double squareSize = pane.getHeight() / Board.getBoard().BOARD_SIZE;
+        this.setHeight(squareSize);
+        this.setWidth(squareSize);
+        this.setX(square.getX() * squareSize);
+        this.setY(square.getY() * squareSize);
+        this.setFill(square.getColor() == Color.BLACK ? GraphicColor.BLACK_SQUARE.getColor() : GraphicColor.WHITE_SQUARE.getColor());
     }
 }
