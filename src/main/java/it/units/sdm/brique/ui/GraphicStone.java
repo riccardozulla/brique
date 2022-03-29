e4ead9aaf6a766e4913274e7a7d1c688dea84930
Antonio Procentese <anto.proce@gmail.com>
2022-03-29 15:52:25 +0200

    GraphicStone class added

---
+++ src/main/java/it/units/sdm/brique/ui/GraphicStone.java
@@ -0,0 +1,11 @@
+package it.units.sdm.brique.ui;
+
+import it.units.sdm.brique.model.Stone;
+import javafx.scene.shape.Ellipse;
+
+public class GraphicStone extends Ellipse {
+
+    public GraphicStone(Stone stone){
+
+    }
+}
