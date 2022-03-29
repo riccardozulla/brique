f545b3c63b2dffa31ae504b4af80a1838812699c
riccardozulla <riccardozulla@gmail.com>
2022-03-29 17:08:43 +0200

    implemented ctor and draw methods in GraphicBoard class

--- src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
@@ -1,13 +1,27 @@
 package it.units.sdm.brique.ui;
 
 import it.units.sdm.brique.model.Board;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Box;
 
-public class GraphicBoard extends Box {
+import java.util.Arrays;
+
+public class GraphicBoard extends Box implements Drawable {
 
   Board board;
+  GraphicSquare[][] graphicSquares;
 
   public GraphicBoard() {
     this.board = Board.getBoard();
+    graphicSquares = new GraphicSquare[Board.BOARD_SIZE][Board.BOARD_SIZE];
+    for (int i = 0; i < Board.BOARD_SIZE; i++) {
+      for (int j = 0; j < Board.BOARD_SIZE; j++) {
+        graphicSquares[i][j] = new GraphicSquare(board.getSquare(i, j));
+      }
+    }
+  }
+
+  public void draw(Pane pane) {
+    Arrays.stream(graphicSquares).flatMap(Arrays::stream).forEach(graphicSquare -> graphicSquare.drawSquare(pane));
   }
 }
