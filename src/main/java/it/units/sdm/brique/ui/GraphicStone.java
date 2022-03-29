2f5b6fdb7a9be39d26901273395cd7b2c02ff812
Antonio Procentese <anto.proce@gmail.com>
2022-03-29 16:27:48 +0200

    GraphicStone constructor added (left a temporary comment to be removed)

--- src/main/java/it/units/sdm/brique/ui/GraphicStone.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicStone.java
@@ -1,11 +1,14 @@
 package it.units.sdm.brique.ui;
 
+import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Stone;
 import javafx.scene.shape.Ellipse;
 
 public class GraphicStone extends Ellipse {
-
-    public GraphicStone(Stone stone){
-
+    Stone stone;
+    public GraphicStone(Stone stone) {
+        this.stone = stone;
+//        this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor()
+//                : GraphicColor.BLACK_STONE.getColor());
     }
 }
