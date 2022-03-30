238f923a03cad9d92bece2a15ccab2e0d1e28b36
Antonio Procentese <anto.proce@gmail.com>
2022-03-30 15:01:20 +0200

    draw method of GraphicStone has been properly overridden

--- src/main/java/it/units/sdm/brique/ui/GraphicStone.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicStone.java
@@ -1,14 +1,27 @@
 package it.units.sdm.brique.ui;
 
+import it.units.sdm.brique.model.Board;
 import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Stone;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.NumberBinding;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Ellipse;
 
-public class GraphicStone extends Ellipse {
+public class GraphicStone extends Ellipse implements Drawable {
     Stone stone;
+
     public GraphicStone(Stone stone) {
         this.stone = stone;
-//        this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor()
-//                : GraphicColor.BLACK_STONE.getColor());
+    }
+
+    @Override
+    public void draw(Pane pane) {
+        NumberBinding squareSize = Bindings.min(pane.heightProperty(), pane.widthProperty()).divide(Board.BOARD_SIZE);
+        this.radiusXProperty().bind(squareSize.multiply(0.31));
+        this.radiusYProperty().bind(squareSize.multiply(0.26));
+        this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor()
+                : GraphicColor.BLACK_STONE.getColor());
+        pane.getChildren().add(this);
     }
 }
