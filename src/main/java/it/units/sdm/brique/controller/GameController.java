084fe88f7111e234c5f14686da61f0449058d61a
riccardozulla <riccardozulla@gmail.com>
2022-03-31 19:54:46 +0200

    version temporary simplified

--- src/main/java/it/units/sdm/brique/controller/GameController.java
+++ src/main/java/it/units/sdm/brique/controller/GameController.java
@@ -6,7 +6,8 @@ import it.units.sdm.brique.model.Player;
 import it.units.sdm.brique.ui.GraphicBoard;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.Pos;
+import javafx.scene.layout.TilePane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,11 +17,13 @@ public class GameController implements Initializable {
   Game game = new Game(new Player("PLayer1", Color.BLACK), new Player("Player2", Color.WHITE));
 
   @FXML
-  private AnchorPane gameView;
+  private TilePane gameView;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     GraphicBoard graphicBoard = new GraphicBoard();
     graphicBoard.draw(gameView);
+    gameView.setPrefSize(400, 400);
+    gameView.setAlignment(Pos.CENTER);
   }
 }
