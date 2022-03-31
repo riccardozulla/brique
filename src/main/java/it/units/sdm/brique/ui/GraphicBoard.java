21af62dc06412f1085a42ca0bf463e502c6660d9
riccardozulla <riccardozulla@gmail.com>
2022-03-31 19:51:30 +0200

    now, GraphicBoard extends Pane

--- src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
@@ -2,11 +2,10 @@ package it.units.sdm.brique.ui;
 
 import it.units.sdm.brique.model.Board;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Box;
 
 import java.util.Arrays;
 
-public class GraphicBoard extends Box implements Drawable {
+public class GraphicBoard extends Pane implements Drawable {
 
   Board board;
   GraphicSquare[][] graphicSquares;
@@ -16,12 +15,13 @@ public class GraphicBoard extends Box implements Drawable {
     graphicSquares = new GraphicSquare[Board.BOARD_SIZE][Board.BOARD_SIZE];
     for (int i = 0; i < Board.BOARD_SIZE; i++) {
       for (int j = 0; j < Board.BOARD_SIZE; j++) {
-        graphicSquares[i][j] = new GraphicSquare(board.getSquare(i, j));
+        this.getChildren().add(new GraphicSquare(board.getSquare(i, j)));
       }
     }
   }
 
   public void draw(Pane pane) {
-    Arrays.stream(graphicSquares).flatMap(Arrays::stream).forEach(graphicSquare -> graphicSquare.draw(pane));
+    this.getChildren().forEach(graphicSquare -> ((GraphicSquare) graphicSquare).draw(pane));
+    pane.getChildren().add(this);
   }
 }
