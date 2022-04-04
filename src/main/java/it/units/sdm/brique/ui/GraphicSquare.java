895d52b53aed1e75ba1f40f2016e11626c88ba78
riccardozulla <riccardozulla@gmail.com>
2022-04-04 17:57:29 +0200

    added eraseStone and drawStone methods

--- src/main/java/it/units/sdm/brique/ui/GraphicSquare.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicSquare.java
@@ -7,24 +7,35 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.binding.NumberBinding;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Rectangle;
 
 public class GraphicSquare extends StackPane implements Drawable {
 
-  private final Square square;
+    private final Square square;
 
-  public GraphicSquare(Square square) {
-    super();
-    this.square = square;
-  }
+    public GraphicSquare(Square square) {
+        super();
+        this.square = square;
+    }
 
-  @Override
-  public void draw(Pane pane) {
-    NumberBinding squareSize = Bindings.min(pane.heightProperty(), pane.widthProperty()).divide(Board.BOARD_SIZE);
-    this.prefHeightProperty().bind(squareSize);
-    this.prefWidthProperty().bind(squareSize);
-    this.layoutXProperty().bind(squareSize.multiply(square.getX()));
-    this.layoutYProperty().bind(squareSize.multiply(square.getY()));
-    this.setStyle("-fx-background-color:" + (square.getColor() == Color.BLACK ? GraphicColor.BLACK_SQUARE.getHexColor() : GraphicColor.WHITE_SQUARE.getHexColor()));
-  }
+    @Override
+    public void draw(Pane pane) {
+        NumberBinding squareSize = Bindings.min(pane.heightProperty(), pane.widthProperty()).divide(Board.BOARD_SIZE);
+        this.prefHeightProperty().bind(squareSize);
+        this.prefWidthProperty().bind(squareSize);
+        this.layoutXProperty().bind(squareSize.multiply(square.getX()));
+        this.layoutYProperty().bind(squareSize.multiply(square.getY()));
+        this.setStyle("-fx-background-color:" + (square.getColor() == Color.BLACK ? GraphicColor.BLACK_SQUARE.getHexColor() : GraphicColor.WHITE_SQUARE.getHexColor()));
+    }
+
+    private void eraseStone() {
+        this.getChildren().clear();
+    }
+
+    private void drawStone() {
+        square.getStone().ifPresent(stone -> {
+            GraphicStone graphicStone = new GraphicStone(stone);
+            graphicStone.draw(this);
+            this.getChildren().add(graphicStone);
+        });
+    }
 }
