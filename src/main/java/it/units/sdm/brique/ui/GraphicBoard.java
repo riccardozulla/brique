e8f87a39a0dd7feafc026ff8c53d455f3931f686
riccardozulla <riccardozulla@gmail.com>
2022-04-04 17:59:30 +0200

    added update method in GraphicBoard class

--- src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
@@ -3,25 +3,25 @@ package it.units.sdm.brique.ui;
 import it.units.sdm.brique.model.Board;
 import javafx.scene.layout.Pane;
 
-import java.util.Arrays;
-
 public class GraphicBoard extends Pane implements Drawable {
 
-  Board board;
-  GraphicSquare[][] graphicSquares;
+    Board board;
 
-  public GraphicBoard() {
-    this.board = Board.getBoard();
-    graphicSquares = new GraphicSquare[Board.BOARD_SIZE][Board.BOARD_SIZE];
-    for (int i = 0; i < Board.BOARD_SIZE; i++) {
-      for (int j = 0; j < Board.BOARD_SIZE; j++) {
-        this.getChildren().add(new GraphicSquare(board.getSquare(i, j)));
-      }
+    public GraphicBoard() {
+        this.board = Board.getBoard();
+        for (int i = 0; i < Board.BOARD_SIZE; i++) {
+            for (int j = 0; j < Board.BOARD_SIZE; j++) {
+                this.getChildren().add(new GraphicSquare(board.getSquare(i, j)));
+            }
+        }
     }
-  }
 
-  public void draw(Pane pane) {
-    this.getChildren().forEach(graphicSquare -> ((GraphicSquare) graphicSquare).draw(pane));
-    pane.getChildren().add(this);
-  }
+    public void draw(Pane pane) {
+        this.getChildren().forEach(graphicSquare -> ((GraphicSquare) graphicSquare).draw(pane));
+        pane.getChildren().add(this);
+    }
+
+    public void update() {
+        this.getChildren().forEach(graphicSquare -> ((GraphicSquare) graphicSquare).update());
+    }
 }
