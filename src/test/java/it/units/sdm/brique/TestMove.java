1a8eaf0935dce8ba504dcac57b273e29e49ca868
riccardozulla <riccardozulla@gmail.com>
2022-04-08 11:20:06 +0200

    placeStoneInOccupiedSquare test added

--- src/test/java/it/units/sdm/brique/TestMove.java
+++ src/test/java/it/units/sdm/brique/TestMove.java
@@ -1,4 +1,17 @@
 package it.units.sdm.brique;
 
+import it.units.sdm.brique.model.*;
+import it.units.sdm.brique.model.exceptions.StoneAlreadyPresentException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
 public class TestMove {
+
+    @Test
+    void placeStoneInOccupiedSquare() {
+        Board.getBoard().getSquare(0, 0).setStone(new Stone(Color.BLACK));
+        Move move = new Move(new Player("Player", Color.BLACK));
+        assertThrowsExactly(StoneAlreadyPresentException.class, () -> move.setChosenSquare(Board.getBoard().getSquare(0, 0)));
+    }
 }
