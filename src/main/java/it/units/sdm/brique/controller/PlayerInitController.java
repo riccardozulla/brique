1b738f4d7d4284fe288c8ce19c16745fa4974589
Alnu73 <axel8744@gmail.com>
2022-04-11 18:13:52 +0200

    Renamed LoginController to PlayerInitController, along with fxml file

--- src/main/java/it/units/sdm/brique/controller/PlayerInitController.java
+++ src/main/java/it/units/sdm/brique/controller/PlayerInitController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable {
+public class PlayerInitController implements Initializable {
 
     @FXML
     private TextField player1TextField;
