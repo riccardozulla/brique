002b9b5ae872f8ff40491e83d8f811bd3f2bb103
Alnu73 <axel8744@gmail.com>
2022-04-11 16:37:09 +0200

    Added login ui and controller

--- src/main/java/it/units/sdm/brique/Brique.java
+++ src/main/java/it/units/sdm/brique/Brique.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Brique extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         Scene scene = new Scene(root);
 
         stage.setTitle("Brique");
