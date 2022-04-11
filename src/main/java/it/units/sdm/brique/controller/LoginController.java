4958d89f07375756c965cf768eaec88dc966cb64
Alnu73 <axel8744@gmail.com>
2022-04-11 16:53:40 +0200

    Added button handler

--- src/main/java/it/units/sdm/brique/controller/LoginController.java
+++ src/main/java/it/units/sdm/brique/controller/LoginController.java
@@ -1,13 +1,30 @@
 package it.units.sdm.brique.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    @FXML
+    private TextField player1TextField;
+    @FXML
+    private TextField player2TextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML protected void handleConfirmButtonAction(ActionEvent event) {
+        System.out.println("Name entered 1: " + player1TextField.getText());
+        System.out.println("Name entered 1: " + player2TextField.getText());
     }
 }
