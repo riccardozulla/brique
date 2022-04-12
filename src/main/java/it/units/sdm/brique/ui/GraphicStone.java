45909d2bf01b6f1ca0335ced6c8cc6ae91b33bb7
Antonio Procentese <anto.proce@gmail.com>
2022-04-12 17:40:07 +0200

    Each stone has now a shadow effect

--- src/main/java/it/units/sdm/brique/ui/GraphicStone.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicStone.java
@@ -4,6 +4,7 @@ import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Stone;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.NumberBinding;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Ellipse;
 
@@ -23,5 +24,11 @@ public class GraphicStone extends Ellipse implements Drawable {
         this.centerYProperty().bind((squareSize.divide(2)));
         this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor()
                 : GraphicColor.BLACK_STONE.getColor());
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setOffsetX(1.0);
+        dropShadow.setOffsetY(8.0);
+        dropShadow.setColor(stone.getColor() == Color.BLACK ? javafx.scene.paint.Color.BLACK
+                : javafx.scene.paint.Color.color(0.05, 0.05,0.05));
+        this.setEffect(dropShadow);
     }
 }
