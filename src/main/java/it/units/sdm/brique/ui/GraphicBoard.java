aa613f6da1064a494dc27b1b9a821d045d43cc0f
riccardozulla <riccardozulla@gmail.com>
2022-04-20 10:22:31 +0200

    GraphicBoard has fixed size too

--- src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
@@ -3,26 +3,23 @@ package it.units.sdm.brique.ui;
 import it.units.sdm.brique.model.Board;
 import javafx.scene.layout.Pane;
 
-public class GraphicBoard extends Pane implements Drawable {
+public class GraphicBoard extends Pane {
 
+    public static final int DEFAULT_BORDER_WIDTH = GraphicSquare.SQUARE_SIZE / 4;
+    public static final int DEFAULT_OFFSET_X = GraphicBoard.DEFAULT_BORDER_WIDTH;
+    public static final int DEFAULT_OFFSET_Y = GraphicBoard.DEFAULT_BORDER_WIDTH;
     Board board;
 
     public GraphicBoard() {
         this.board = Board.getBoard();
         for (int i = 0; i < Board.BOARD_SIZE; i++) {
             for (int j = 0; j < Board.BOARD_SIZE; j++) {
-                this.getChildren().add(new GraphicSquare(board.getSquare(i, j)));
+                GraphicSquare graphicSquare = new GraphicSquare(board.getSquare(i, j));
+                graphicSquare.setLayoutX(i * GraphicSquare.SQUARE_SIZE + DEFAULT_OFFSET_X);
+                graphicSquare.setLayoutY(j * GraphicSquare.SQUARE_SIZE + DEFAULT_OFFSET_Y);
+                this.getChildren().add(graphicSquare);
             }
         }
-    }
-
-    public void draw(Pane pane) {
-        this.heightProperty().addListener((obs, oldVal, newVal) -> {
-            var squareSize = newVal.doubleValue() / Board.BOARD_SIZE;
-            this.setStyle("-fx-border-color:" + GraphicColor.BLACK_STONE.getHexColor() + " " + GraphicColor.WHITE_STONE.getHexColor() + " " + GraphicColor.BLACK_STONE.getHexColor() + " " + GraphicColor.WHITE_STONE.getHexColor() + "; -fx-border-style:solid outside; -fx-border-radius:" + squareSize / 2 + " ; -fx-border-width:" + squareSize + ";"); //todo
-        });
-        this.getChildren().forEach(graphicSquare -> ((GraphicSquare) graphicSquare).draw(pane));
-        pane.getChildren().add(this);
     }
 
     public void update() {
