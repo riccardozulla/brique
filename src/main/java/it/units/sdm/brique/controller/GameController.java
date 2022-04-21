9996795f1ab2288a521f971f00571e6a804cfd56
riccardozulla <riccardozulla@gmail.com>
2022-04-21 17:20:42 +0200

    highlightActivePlayer method added

--- src/main/java/it/units/sdm/brique/controller/GameController.java
+++ src/main/java/it/units/sdm/brique/controller/GameController.java
@@ -7,8 +7,10 @@ import it.units.sdm.brique.ui.GraphicBoard;
 import it.units.sdm.brique.ui.GraphicSquare;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -18,6 +20,18 @@ import java.util.ResourceBundle;
 public class GameController implements Initializable, PropertyChangeListener {
 
     Game game = new Game(new Player("PLayer1", Color.BLACK), new Player("Player2", Color.WHITE));
+
+    @FXML
+    private VBox player_one_wrapper;
+    @FXML
+    private VBox player_two_wrapper;
+    @FXML
+    private Text player_one_nickname;
+    @FXML
+    private Text player_two_nickname;
+
+    @FXML
+    private Button pie_button;
 
     @FXML
     private VBox game_wrapper;
@@ -32,7 +46,7 @@ public class GameController implements Initializable, PropertyChangeListener {
     public void initialize(URL location, ResourceBundle resources) {
 
         Image background = new Image("it/units/sdm/brique/game_carpet.jpg");
-        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, new BackgroundSize(300,300,false, false, false, false));
+        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, new BackgroundSize(300, 300, false, false, false, false));
         game_wrapper.setBackground(new Background(backgroundImage));
 
         board_wrapper.layoutBoundsProperty().addListener(graphicBoard.fit);
@@ -44,17 +58,32 @@ public class GameController implements Initializable, PropertyChangeListener {
 
         game.addActivePlayerListener(this);
         game.addStatusListener(this);
+        highlightActivePlayer();
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
         switch (propertyChangeEvent.getPropertyName()) {
-            case "activePlayer" -> graphicBoard.update();
+            case "activePlayer" -> {
+                graphicBoard.update();
+                highlightActivePlayer();
+            }
             case "status" -> {
                 System.out.println(propertyChangeEvent.getNewValue());
                 graphicBoard.update();
                 graphicBoard.setDisable(true);
             }
+        }
+    }
+
+
+    private void highlightActivePlayer() {
+        if (game.getPlayer1().equals(game.getActivePlayer())) {
+            player_one_wrapper.getStyleClass().add("activePlayer");
+            player_two_wrapper.getStyleClass().remove("activePlayer");
+        } else {
+            player_two_wrapper.getStyleClass().add("activePlayer");
+            player_one_wrapper.getStyleClass().remove("activePlayer");
         }
     }
 }
