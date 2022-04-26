d187b3de53bb52493319fef1067828a3d784402a
Antonio Procentese <anto.proce@gmail.com>
2022-04-26 16:10:47 +0200

    getLeftReturnsOptionalEmpty is now parametrized

--- src/test/java/it/units/sdm/brique/TestBoard.java
+++ src/test/java/it/units/sdm/brique/TestBoard.java
@@ -75,9 +75,10 @@ public class TestBoard {
     assertEquals(Optional.empty(), board.getRight(board.getSquare(i, j)));
   }
 
-  @Test
-  void getLeftReturnsOptionalEmpty() {
-    assertEquals(Optional.empty(), board.getLeft(board.getSquare(0, 0)));
+  @ParameterizedTest
+  @CsvSource({"0,0", "1,0", "2,0", "3,0", "4,0", "5,0", "6,0","7,0","8,0","9,0","10,0","11,0","12,0","13,0","14,0"})
+  void getLeftReturnsOptionalEmpty(int i, int j) {
+    assertEquals(Optional.empty(), board.getLeft(board.getSquare(i, j)));
   }
 
   @Test
