acb658d5467f84b696ee1b9b83acb7430f2f3bcf
Antonio Procentese <anto.proce@gmail.com>
2022-04-26 17:33:00 +0200

    added lastRowSquaresAreOnTheBottomEdge in TestSquare

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -50,4 +50,11 @@ public class TestSquare {
         Square topEdgeSquare = new Square(i,j);
         assertTrue(topEdgeSquare.isTopEdge());
     }
+
+    @ParameterizedTest
+    @CsvSource({"14,0", "14,1", "14,2", "14,3","14,4", "14,5","14,6","14,7","14,8","14,9","14,10","14,11","14,12","14,13","14,14"})
+    void lastRowSquaresAreOnTheBottomEdge(int i, int j){
+        Square bottomEdgeSquare = new Square(i,j);
+        assertTrue(bottomEdgeSquare.isBottomEdge());
+    }
 }
