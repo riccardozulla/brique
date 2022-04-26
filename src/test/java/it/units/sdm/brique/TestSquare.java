c884d6ec3cdc06d136a473723b480b90916dabe0
Antonio Procentese <anto.proce@gmail.com>
2022-04-26 17:21:44 +0200

    Naming refactoring of tests in TestSquare and manhattanDistanceBetweenAdjacentOrthogonalSquareIsOne test added

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -4,6 +4,8 @@ import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Square;
 import it.units.sdm.brique.model.Stone;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,13 +13,20 @@ public class TestSquare {
     private final Square square = new Square(0,0);
 
     @Test
-    void toggleTestToTrue() {
+    void settingStoneOnSquareMakesGetStonePresent() {
         square.setStone(new Stone(Color.BLACK));
         assert(square.getStone().isPresent());
     }
 
     @Test
-    void checkTestFalse() {
+    void emptySquareHasNotStoneOnIt() {
         assertFalse(square.getStone().isPresent());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,1,1,2", "2,2,2,3", "3,3,3,4", "4,4,4,5", "5,5,5,6","6,6,6,7","7,7,7,8","8,8,8,9","9,9,9,10", "10,10,10,11"})
+    void manhattanDistanceBetweenAdjacentOrthogonalSquareIsOne(int i, int j, int k, int l)
+    {
+        assertEquals(1, Square.manhattanSquareDistance(new Square(i,j), new Square(k,l)));
     }
 }
