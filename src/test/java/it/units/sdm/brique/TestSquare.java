4e04844b950c3e6dad8cf5d9a486aad2264d247d
Antonio Procentese <anto.proce@gmail.com>
2022-04-26 17:29:45 +0200

    added firstColSquaresAreOnTheLeftEdge in TestSquare

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -36,4 +36,11 @@ public class TestSquare {
         Square rightEdgeSquare = new Square(i,j);
         assertTrue(rightEdgeSquare.isRightEdge());
     }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "1,0", "2,0", "3,0","4,0","5,0","6,0","7,0","8,0","9,0","10,0","11,0","12,0","13,0","14,0"})
+    void firstColSquaresAreOnTheLeftEdge(int i, int j){
+        Square leftEdgeSquare = new Square(i,j);
+        assertTrue(leftEdgeSquare.isLeftEdge());
+    }
 }
