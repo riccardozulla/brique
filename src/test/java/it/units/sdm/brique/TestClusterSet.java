17a5cfb5a27f905b6ee83ab27ccf0c09d882b8be
Antonio Procentese <anto.proce@gmail.com>
2022-04-27 17:25:29 +0200

    Refactored tests in TestClusterSet (functional style)

--- src/test/java/it/units/sdm/brique/TestClusterSet.java
+++ src/test/java/it/units/sdm/brique/TestClusterSet.java
@@ -6,8 +6,11 @@ import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Square;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -29,10 +32,7 @@ public class TestClusterSet {
 
     @Test
     void whiteEdgeToEdgePathIsWinning() {
-        Set<Square> squareSet = new HashSet<>();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            squareSet.add(Board.getBoard().getSquare(1, i));
-        }
+        Set<Square> squareSet = IntStream.range(0, Board.BOARD_SIZE).mapToObj(i -> Board.getBoard().getSquare(1, i)).collect(Collectors.toSet());
         ClusterSet clusterSet = new ClusterSet(squareSet, Color.WHITE);
         clusterSet.composeClusters();
         assertEquals(1, clusterSet.numberOfSets());
@@ -41,10 +41,7 @@ public class TestClusterSet {
 
     @Test
     void blackEdgeToEdgePathIsWinning() {
-        Set<Square> squareSet = new HashSet<>();
-        for (int i = 0; i < Board.BOARD_SIZE; i++) {
-            squareSet.add(Board.getBoard().getSquare(i, 1));
-        }
+        Set<Square> squareSet = IntStream.range(0, Board.BOARD_SIZE).mapToObj(i -> Board.getBoard().getSquare(i, 1)).collect(Collectors.toSet());
         ClusterSet clusterSet = new ClusterSet(squareSet, Color.BLACK);
         clusterSet.composeClusters();
         assertEquals(1, clusterSet.numberOfSets());
