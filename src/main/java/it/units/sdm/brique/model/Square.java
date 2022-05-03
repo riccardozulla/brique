e4b8f5a62c4a26f8b2be1dfe8eae23551f2060ae
riccardozulla <riccardozulla@gmail.com>
2022-05-03 17:02:04 +0200

    formatted Square class

--- src/main/java/it/units/sdm/brique/model/Square.java
+++ src/main/java/it/units/sdm/brique/model/Square.java
@@ -3,60 +3,64 @@ package it.units.sdm.brique.model;
 import java.util.Optional;
 
 public class Square {
-  private final Color color;
-  private Stone stone;
-  private final int row;
-  private final int column;
+    private final Color color;
+    private Stone stone;
+    private final int row;
+    private final int column;
 
-  public Square(int row, int column) {
-    this.row = row;
-    this.column = column;
-    this.color = (row + column) % 2 == 0 ? Color.WHITE : Color.BLACK;
-  }
+    public Square(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.color = (row + column) % 2 == 0 ? Color.WHITE : Color.BLACK;
+    }
 
-  public Color getColor() {
-    return color;
-  }
+    public Color getColor() {
+        return color;
+    }
 
-  public Optional<Stone> getStone() {
-    return Optional.ofNullable(stone);
-  }
+    public Optional<Stone> getStone() {
+        return Optional.ofNullable(stone);
+    }
 
-  public int getRow() {
-    return row;
-  }
+    public int getRow() {
+        return row;
+    }
 
-  public int getColumn() { return column; }
+    public int getColumn() {
+        return column;
+    }
 
-  public boolean isTopEdge() {
-    return row ==Board.FIRST_INDEX;
-  }
+    public boolean isTopEdge() {
+        return row == Board.FIRST_INDEX;
+    }
 
-  public boolean isBottomEdge() {
-    return row ==Board.LAST_INDEX;
-  }
+    public boolean isBottomEdge() {
+        return row == Board.LAST_INDEX;
+    }
 
-  public boolean isLeftEdge(){ return column ==Board.FIRST_INDEX; }
+    public boolean isLeftEdge() {
+        return column == Board.FIRST_INDEX;
+    }
 
-  public boolean isRightEdge(){
-    return column ==Board.LAST_INDEX;
-  }
+    public boolean isRightEdge() {
+        return column == Board.LAST_INDEX;
+    }
 
-  public void setStone(Stone stone) {
-    this.stone = stone;
-  }
+    public void setStone(Stone stone) {
+        this.stone = stone;
+    }
 
-  public void removeStone(){
-    stone = null;
-  }
+    public void removeStone() {
+        stone = null;
+    }
 
-  public boolean isOccupied() {
-    return stone!=null;
-  }
+    public boolean isOccupied() {
+        return stone != null;
+    }
 
-  public static int manhattanSquareDistance(Square square1, Square square2) {
-    return Math.abs(square1.getRow() - square2.getRow())
-            + Math.abs(square1.getColumn() - square2.getColumn());
-  }
+    public static int manhattanSquareDistance(Square square1, Square square2) {
+        return Math.abs(square1.getRow() - square2.getRow())
+                + Math.abs(square1.getColumn() - square2.getColumn());
+    }
 }
 
