0c511c35d1b9ad2d1c38e83a4254e71bee715bbc
Antonio Procentese <anto.proce@gmail.com>
2022-05-03 11:40:04 +0200

    method names refactoring in TestBoard

--- src/test/java/it/units/sdm/brique/TestBoard.java
+++ src/test/java/it/units/sdm/brique/TestBoard.java
@@ -32,73 +32,73 @@ public class TestBoard {
 
   @ParameterizedTest
   @CsvSource({"0,1,1,1","1,2,2,2", "2,3,3,3", "3,4,4,4","4,5,5,5", "5,6,6,6", "6,7,7,7", "7,8,8,8", "8,9,9,9", "9,10,10,10", "10,11,11,11"})
-  void getUp(int actualRow, int actualCol, int upRow, int upCol) {
+  void getUpSquare(int actualRow, int actualCol, int upRow, int upCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getUpSquare(board.getSquare(upRow, upCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"2,1,1,1","3,2,2,2", "4,3,3,3", "5,4,4,4","6,5,5,5", "7,6,6,6", "8,7,7,7", "9,8,8,8", "10,9,9,9", "11,10,10,10", "12,11,11,11"})
-  void getDown(int actualRow, int actualCol, int downRow, int downCol) {
+  void getDownSquare(int actualRow, int actualCol, int downRow, int downCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getDownSquare(board.getSquare(downRow, downCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"1,0,1,1","2,1,2,2", "3,2,3,3", "4,3,4,4","5,4,5,5", "6,5,6,6", "7,6,7,7", "8,7,8,8", "9,8,9,9", "10,9,10,10", "11,10,11,11"})
-  void getLeft(int actualRow, int actualCol, int rightRow, int rightCol) {
+  void getLeftSquare(int actualRow, int actualCol, int rightRow, int rightCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getLeftSquare(board.getSquare(rightRow, rightCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"1,2,1,1","2,3,2,2", "3,4,3,3", "4,5,4,4","5,6,5,5", "6,7,6,6", "7,8,7,7", "8,9,8,8", "9,10,9,9", "10,11,10,10", "11,12,11,11"})
-          void getRight(int actualRow, int actualCol, int rightRow, int rightCol) {
+          void getRightSquare(int actualRow, int actualCol, int rightRow, int rightCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getRightSquare(board.getSquare(rightRow, rightCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"2,0,1,1", "3,1,2,2", "4,2,3,3", "5,3,4,4","6,4,5,5", "7,5,6,6", "8,6,7,7", "9,7,8,8", "9,7,8,8", "10,8,9,9", "11,9,10,10"})
-  void getDownLeft(int actualRow, int actualCol, int downLeftRow, int downLeftCol) {
+  void getDownLeftSquare(int actualRow, int actualCol, int downLeftRow, int downLeftCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getDownLeftSquare(board.getSquare(downLeftRow, downLeftCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"0,2,1,1", "1,1,2,0", "3,1,4,0", "4,2,5,1","5,3,6,2", "6,4,7,3", "7,5,8,4", "8,6,9,5", "9,7,10,6", "10,8,11,7", "11,9,12,8"})
-  void getUpRight(int actualRow, int actualCol, int upRightRow, int upRightCol) {
+  void getUpRightSquare(int actualRow, int actualCol, int upRightRow, int upRightCol) {
     assertEquals(board.getSquare(actualRow, actualCol), board.getUpRightSquare(board.getSquare(upRightRow, upRightCol)));
   }
 
   @ParameterizedTest
   @CsvSource({"0,0", "0,1", "0,2", "0,3", "0,4", "0,5", "0,6","0,7","0,8","0,9","0,10","0,11","0,12","0,13","0,14"})
-  void getUpReturnsOptionalEmpty(int i, int j) {
+  void getUpSquareThrowsExceptionIfSquareIsOnTopEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getUpSquare(board.getSquare(i,j)));
   }
 
   @ParameterizedTest
   @CsvSource({"14,0", "14,1", "14,2", "14,3", "14,4", "14,5", "14,6","14,7","14,8","14,9","14,10","14,11","14,12","14,13","14,14"})
-  void getDownThrowsExceptionIfSquareIsOnBottomEdge(int i, int j) {
+  void getDownSquareThrowsExceptionIfSquareIsOnBottomEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getDownSquare(board.getSquare(i, j)));
       }
 
   @ParameterizedTest
   @CsvSource({"0,14", "1,14", "2,14", "3,14", "4,14", "5,14", "6,14","7,14","8,14","9,14","10,14","11,14","12,14","13,14","14,14"})
-  void getRightThrowsExceptionIfSquareIsOnRightEdge(int i, int j) {
+  void getRightSquareThrowsExceptionIfSquareIsOnRightEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getRightSquare(board.getSquare(i, j)));
   }
 
   @ParameterizedTest
   @CsvSource({"0,0", "1,0", "2,0", "3,0", "4,0", "5,0", "6,0","7,0","8,0","9,0","10,0","11,0","12,0","13,0","14,0"})
-  void getLeftThrowsExceptionIfSquareIsOnLeftEdge(int i, int j) {
+  void getLeftSquareThrowsExceptionIfSquareIsOnLeftEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getLeftSquare(board.getSquare(i, j)));
   }
 
   @ParameterizedTest
   @CsvSource({"0,0", "0,1", "0,2", "0,3", "0,4", "0,5", "0,6","0,7","0,8","0,9","0,10","0,11","0,12","0,13","0,14","1,14", "2,14", "3,14", "4,14", "5,14", "6,14","7,14","8,14","9,14","10,14","11,14","12,14","13,14","14,14"})
-  void getUpRightThrowsExceptionIfSquareIsOnTopOrRightEdge(int i, int j) {
+  void getUpRightSquareThrowsExceptionIfSquareIsOnTopOrRightEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getUpRightSquare(board.getSquare(i, j)));
   }
 
   @ParameterizedTest
   @CsvSource({"0,0", "1,0", "2,0", "3,0", "4,0", "5,0", "6,0","7,0","8,0","9,0","10,0","11,0","12,0","13,0","14,0","14,1", "14,2", "14,3", "14,4", "14,5", "14,6","14,7","14,8","14,9","14,10","14,11","14,12","14,13","14,14"})
-  void getDownLeftThrowsExceptionIfSquareIsOnLeftOrBottomEdge(int i, int j) {
+  void getDownLeftSquareThrowsExceptionIfSquareIsOnLeftOrBottomEdge(int i, int j) {
     assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> board.getDownLeftSquare(board.getSquare(i, j)));
   }
 }
