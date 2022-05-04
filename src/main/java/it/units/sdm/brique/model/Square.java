b12ecf14448da8fad13a7ef3f4e81039f56b7648
riccardozulla <riccardozulla@gmail.com>
2022-05-04 14:38:56 +0200

    formatted class

--- src/main/java/it/units/sdm/brique/model/Square.java
+++ src/main/java/it/units/sdm/brique/model/Square.java
@@ -3,15 +3,27 @@ package it.units.sdm.brique.model;
 import java.util.Optional;
 
 public class Square {
-    private final Color color;
-    private Stone stone;
     private final int row;
     private final int column;
+    private final Color color;
+    private Stone stone;
 
     public Square(int row, int column) {
         this.row = row;
         this.column = column;
         this.color = (row + column) % 2 == 0 ? Color.WHITE : Color.BLACK;
+    }
+
+    public static int manhattanSquareDistance(Square square1, Square square2) {
+        return Math.abs(square1.getRow() - square2.getRow()) + Math.abs(square1.getColumn() - square2.getColumn());
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
     }
 
     public Color getColor() {
@@ -22,12 +34,16 @@ public class Square {
         return Optional.ofNullable(stone);
     }
 
-    public int getRow() {
-        return row;
+    public void setStone(Stone stone) {
+        this.stone = stone;
     }
 
-    public int getColumn() {
-        return column;
+    public void removeStone() {
+        stone = null;
+    }
+
+    public boolean isOccupied() {
+        return stone != null;
     }
 
     public boolean isTopEdge() {
@@ -44,23 +60,6 @@ public class Square {
 
     public boolean isRightEdge() {
         return column == Board.LAST_INDEX;
-    }
-
-    public void setStone(Stone stone) {
-        this.stone = stone;
-    }
-
-    public void removeStone() {
-        stone = null;
-    }
-
-    public boolean isOccupied() {
-        return stone != null;
-    }
-
-    public static int manhattanSquareDistance(Square square1, Square square2) {
-        return Math.abs(square1.getRow() - square2.getRow())
-                + Math.abs(square1.getColumn() - square2.getColumn());
     }
 }
 
