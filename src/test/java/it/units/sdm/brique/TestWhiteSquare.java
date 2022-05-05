b5440c43ea235276b82ccd38d933d6c0088bb83e
riccardozulla <riccardozulla@gmail.com>
2022-05-05 16:25:32 +0200

    added test to check escorts of squares not on edges

--- src/test/java/it/units/sdm/brique/TestWhiteSquare.java
+++ src/test/java/it/units/sdm/brique/TestWhiteSquare.java
@@ -1,8 +1,12 @@
 package it.units.sdm.brique;
 
+import it.units.sdm.brique.model.Board;
 import it.units.sdm.brique.model.Color;
+import it.units.sdm.brique.model.Square;
 import it.units.sdm.brique.model.WhiteSquare;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +15,13 @@ public class TestWhiteSquare {
     @Test
     void color() {
         assertEquals(Color.WHITE, new WhiteSquare(0,0).getColor());
+    }
+
+    @Test
+    void hasExpectedEscortListWhenSquareNotOnEdges() {
+        Board board = Board.getBoard();
+        Square square = board.getSquare(1,1);
+        List<Square> expectedEscortList = List.of(board.getUpSquare(square), board.getLeftSquare(square));
+        assertEquals(expectedEscortList, square.getEscorts());
     }
 }
