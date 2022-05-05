5479e487b3894eb45165833a602bfcbd5abc0200
riccardozulla <riccardozulla@gmail.com>
2022-05-05 17:55:31 +0200

    removed redundant tests

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -49,28 +49,6 @@ public class TestSquare {
         assertFalse(defaultSquare.isOccupied());
     }
 
-    //TODO: move tests to Board class
-//    @ParameterizedTest
-//    @CsvSource({"0,0", "1,1", "2,2", "3,3", "4,4", "5,5", "6,6", "7,7", "8,8", "9,9", "10,10", "11,11", "12,12", "13,13", "14,14", "0,14", "1,13", "2,12", "3,11", "4,10", "5,9", "6,8", "8,6", "9,5", "10,4", "11,3", "12,2", "13,1", "14,0"})
-//    void getColorReturnsWhiteOnTheDiagonals(int i, int j) {
-//        Square square = new Square(i, j);
-//        assertEquals(Color.WHITE, square.getColor());
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({"0,1", "0,3", "0,5", "0,7", "0,9", "0,11", "0,13"})
-//    void getColorReturnsBlackOnTheEvenSquaresOfFirstRow(int i, int j) {
-//        Square square = new Square(i, j);
-//        assertEquals(Color.BLACK, square.getColor());
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({"14,1", "14,3", "14,5", "14,7", "14,9", "14,11", "14,13"})
-//    void getColorReturnsBlackOnTheEvenSquaresOfLastRow(int i, int j) {
-//        Square square = new Square(i, j);
-//        assertEquals(Color.BLACK, square.getColor());
-//    }
-
     @ParameterizedTest
     @CsvSource({"1,1,1,2", "2,2,2,3", "3,3,3,4", "4,4,4,5", "5,5,5,6", "6,6,6,7", "7,7,7,8", "8,8,8,9", "9,9,9,10", "10,10,10,11"})
     void manhattanDistanceBetweenAdjacentOrthogonalSquareIsOne(int i, int j, int k, int l) {
