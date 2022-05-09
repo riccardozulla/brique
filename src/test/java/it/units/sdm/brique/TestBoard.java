17fe1db3a5843d3da87973eb5e3e312eded52d85
riccardozulla <riccardozulla@gmail.com>
2022-05-09 12:20:27 +0200

    added singleSquareWithStoneOnItIsOccupiedAsExpected test

--- src/test/java/it/units/sdm/brique/TestBoard.java
+++ src/test/java/it/units/sdm/brique/TestBoard.java
@@ -3,28 +3,42 @@ package it.units.sdm.brique;
 import it.units.sdm.brique.model.Board;
 import it.units.sdm.brique.model.Color;
 import it.units.sdm.brique.model.Square;
+import it.units.sdm.brique.model.Stone;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestBoard {
-  private final Board board = Board.getBoard();
+    private final Board board = Board.getBoard();
 
-  @Test
-  void numberOfSquaresIsTheExpectedOne(){
-    assertEquals(Board.BOARD_SIZE*Board.BOARD_SIZE, board.getSquaresStream().count());
-  }
+    @Test
+    void numberOfSquaresIsTheExpectedOne() {
+        assertEquals(Board.BOARD_SIZE * Board.BOARD_SIZE, board.getSquaresStream().count());
+    }
 
-  @ParameterizedTest
-  @CsvSource({"0,0,WHITE", "1,1,WHITE", "2,0,WHITE", "1,0,BLACK", "0,1,BLACK", "3,0,BLACK", "14,14,WHITE"})
-  void squareColorIsTheExpectedOne(int x, int y, Color color) {
-    assertEquals(color, board.getSquare(x, y).getColor());
-  }
+    @ParameterizedTest
+    @CsvSource({"0,0,WHITE", "1,1,WHITE", "2,0,WHITE", "1,0,BLACK", "0,1,BLACK", "3,0,BLACK", "14,14,WHITE"})
+    void squareColorIsTheExpectedOne(int x, int y, Color color) {
+        assertEquals(color, board.getSquare(x, y).getColor());
+    }
 
-  @Test
-  void resetBoard() {
-    board.reset();
-    assertFalse(board.getSquaresStream().anyMatch(Square::isOccupied));
-  }
+    @Test
+    void resetBoard() {
+        board.reset();
+        assertFalse(board.getSquaresStream().anyMatch(Square::isOccupied));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,1", "4,7", "9, 2", "11,13"})
+    void singleSquareWithStoneOnItIsOccupiedAsExpected(int i, int j) {
+        board.reset();
+        Square square = board.getSquare(i, j);
+        square.setStone(new Stone(Color.BLACK));
+        assertEquals(List.of(square), board.getOccupiedSquares());
+    }
 }
