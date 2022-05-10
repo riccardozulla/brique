f7b13fbc4af6771c086063cea7331ff9b16aaf01
riccardozulla <riccardozulla@gmail.com>
2022-05-10 17:14:28 +0200

    added StonePlacementWhenGameIsOverException class

---
+++ src/main/java/it/units/sdm/brique/model/exceptions/StonePlacementWhenGameIsOverException.java
@@ -0,0 +1,7 @@
+package it.units.sdm.brique.model.exceptions;
+
+public class StonePlacementWhenGameIsOverException extends RuntimeException {
+    public StonePlacementWhenGameIsOverException(String message) {
+        super(message);
+    }
+}
