a0193f3f9b9c921bddfe0f99247912524b3498b2
riccardozulla <riccardozulla@gmail.com>
2022-05-13 15:53:18 +0200

    removed DEFAULT_OFFSET constant from GraphicBoard class

--- src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicBoard.java
@@ -12,8 +12,7 @@ import javafx.scene.shape.Polygon;
 public class GraphicBoard extends Pane {
 
     public static final int DEFAULT_BORDER_WIDTH = GraphicSquare.SQUARE_SIZE / 4;
-    public static final int DEFAULT_OFFSET = GraphicBoard.DEFAULT_BORDER_WIDTH;
-    public static final int DEFAULT_BOARD_LENGTH = Board.BOARD_SIZE * GraphicSquare.SQUARE_SIZE + 2 * DEFAULT_OFFSET;
+    public static final int DEFAULT_BOARD_LENGTH = Board.BOARD_SIZE * GraphicSquare.SQUARE_SIZE + 2 * DEFAULT_BORDER_WIDTH;
     public static final double PERCENTAGE_PADDING = 0.9;
 
     public final ChangeListener<Bounds> fit = (observable, oldValue, newValue) -> {
@@ -29,8 +28,8 @@ public class GraphicBoard extends Pane {
         for (int row = 0; row < Board.BOARD_SIZE; row++) {
             for (int col = 0; col < Board.BOARD_SIZE; col++) {
                 GraphicSquare graphicSquare = new GraphicSquare(board.getSquare(row, col));
-                graphicSquare.setLayoutX(col * GraphicSquare.SQUARE_SIZE + DEFAULT_OFFSET);
-                graphicSquare.setLayoutY(row * GraphicSquare.SQUARE_SIZE + DEFAULT_OFFSET);
+                graphicSquare.setLayoutX(col * GraphicSquare.SQUARE_SIZE + DEFAULT_BORDER_WIDTH);
+                graphicSquare.setLayoutY(row * GraphicSquare.SQUARE_SIZE + DEFAULT_BORDER_WIDTH);
                 this.getChildren().add(graphicSquare);
             }
         }
@@ -41,25 +40,25 @@ public class GraphicBoard extends Pane {
         Polygon topBorder = new Polygon(
                 0.0, 0.0,
                 DEFAULT_BOARD_LENGTH, 0.0,
-                DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET, DEFAULT_OFFSET,
-                DEFAULT_OFFSET, DEFAULT_OFFSET);
+                DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH, DEFAULT_BORDER_WIDTH,
+                DEFAULT_BORDER_WIDTH, DEFAULT_BORDER_WIDTH);
         topBorder.setFill(GraphicColor.BLACK_STONE.getColor());
         Polygon rightBorder = new Polygon(
                 DEFAULT_BOARD_LENGTH, 0.0,
                 DEFAULT_BOARD_LENGTH, DEFAULT_BOARD_LENGTH,
-                DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET, DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET,
-                DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET, DEFAULT_OFFSET);
+                DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH, DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH,
+                DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH, DEFAULT_BORDER_WIDTH);
         rightBorder.setFill(GraphicColor.WHITE_STONE.getColor());
         Polygon bottomBorder = new Polygon(
-                DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET, DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET,
+                DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH, DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH,
                 DEFAULT_BOARD_LENGTH, DEFAULT_BOARD_LENGTH,
                 0, DEFAULT_BOARD_LENGTH,
-                DEFAULT_OFFSET, DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET);
+                DEFAULT_BORDER_WIDTH, DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH);
         bottomBorder.setFill(GraphicColor.BLACK_STONE.getColor());
         Polygon leftBorder = new Polygon(
                 0.0, 0.0,
-                DEFAULT_OFFSET, DEFAULT_OFFSET,
-                DEFAULT_OFFSET, DEFAULT_BOARD_LENGTH - DEFAULT_OFFSET,
+                DEFAULT_BORDER_WIDTH, DEFAULT_BORDER_WIDTH,
+                DEFAULT_BORDER_WIDTH, DEFAULT_BOARD_LENGTH - DEFAULT_BORDER_WIDTH,
                 0.0, DEFAULT_BOARD_LENGTH);
         leftBorder.setFill(GraphicColor.WHITE_STONE.getColor());
 
