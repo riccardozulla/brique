f3b4c3049d7f658fc8ddec206f851c7bdef51ac9
riccardozulla <riccardozulla@gmail.com>
2022-05-13 15:35:55 +0200

    enhanced GraphicStone constructor

--- src/main/java/it/units/sdm/brique/ui/GraphicStone.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicStone.java
@@ -5,13 +5,13 @@ import it.units.sdm.brique.model.Stone;
 import javafx.scene.shape.Ellipse;
 
 public class GraphicStone extends Ellipse {
+    public static final double STONE_RADIUS_X = GraphicSquare.SQUARE_SIZE * 0.31;
+    public static final double STONE_RADIUS_Y = GraphicSquare.SQUARE_SIZE * 0.26;
     private final Stone stone;
 
     public GraphicStone(Stone stone) {
-        super(GraphicSquare.SQUARE_SIZE / 2.0, GraphicSquare.SQUARE_SIZE / 2.0,
-                GraphicSquare.SQUARE_SIZE * 0.31, GraphicSquare.SQUARE_SIZE * 0.26);
+        super(GraphicSquare.SQUARE_SIZE / 2.0, GraphicSquare.SQUARE_SIZE / 2.0, STONE_RADIUS_X, STONE_RADIUS_Y);
         this.stone = stone;
-        this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor()
-                : GraphicColor.BLACK_STONE.getColor());
+        this.setFill(stone.getColor() == Color.WHITE ? GraphicColor.WHITE_STONE.getColor() : GraphicColor.BLACK_STONE.getColor());
     }
 }
