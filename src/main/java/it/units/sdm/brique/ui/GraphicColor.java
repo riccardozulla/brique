6dddc7d6d84aa8e453182092440ef8ded8af5cab
riccardozulla <riccardozulla@gmail.com>
2022-05-13 15:42:18 +0200

    formatted GraphicColor.java file

--- src/main/java/it/units/sdm/brique/ui/GraphicColor.java
+++ src/main/java/it/units/sdm/brique/ui/GraphicColor.java
@@ -3,19 +3,19 @@ package it.units.sdm.brique.ui;
 import javafx.scene.paint.Paint;
 
 public enum GraphicColor {
-  WHITE_SQUARE("#e0be98"), BLACK_SQUARE("#8a4929"), WHITE_STONE("#ede6e6"), BLACK_STONE("#272b28");
+    WHITE_SQUARE("#e0be98"), BLACK_SQUARE("#8a4929"), WHITE_STONE("#ede6e6"), BLACK_STONE("#272b28");
 
-  private final String hexColor;
+    private final String hexColor;
 
-  GraphicColor(String hexColor) {
-    this.hexColor = hexColor;
-  }
+    GraphicColor(String hexColor) {
+        this.hexColor = hexColor;
+    }
 
-  public String getHexColor() {
-    return hexColor;
-  }
+    public String getHexColor() {
+        return hexColor;
+    }
 
-  public Paint getColor() {
-    return Paint.valueOf(hexColor);
-  }
+    public Paint getColor() {
+        return Paint.valueOf(hexColor);
+    }
 }
