4f804f576d153e55f95bf3f9d698c66741559374
riccardozulla <riccardozulla@gmail.com>
2022-05-14 15:32:49 +0200

    set playerInit view prefSize

--- src/main/java/it/units/sdm/brique/controller/PlayerInitController.java
+++ src/main/java/it/units/sdm/brique/controller/PlayerInitController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
