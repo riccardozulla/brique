25c54a8cf11aa43d00c41e37d1f03d16d9e57940
Antonio Procentese <anto.proce@gmail.com>
2022-05-17 11:52:09 +0200

    manhattanDistanceBetweenAdjacentOrthogonalSquareIsOne --> manhattanDistanceBetweenAdjacentOrthogonalSquaresIsOne in TestSquare

--- src/test/java/it/units/sdm/brique/TestSquare.java
+++ src/test/java/it/units/sdm/brique/TestSquare.java
@@ -51,7 +51,7 @@ public class TestSquare {
 
     @ParameterizedTest
     @CsvSource({"1,1,1,2", "2,2,2,3", "3,3,3,4", "4,4,4,5", "5,5,5,6", "6,6,6,7", "7,7,7,8", "8,8,8,9", "9,9,9,10", "10,10,10,11"})
-    void manhattanDistanceBetweenAdjacentOrthogonalSquareIsOne(int i, int j, int k, int l) {
+    void manhattanDistanceBetweenAdjacentOrthogonalSquaresIsOne(int i, int j, int k, int l) {
         assertEquals(1, Square.manhattanSquareDistance(generateMockSquare(i, j), generateMockSquare(k, l)));
     }
 
